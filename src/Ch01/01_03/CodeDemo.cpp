8e26398f3b0298b664b7476fb0478788eb7ebfcc
wccalvin <clayton@wccalvin.com>
2024-10-07 22:01:32 -0400

    first program

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -1,15 +1,16 @@
-// Learning C++ 
+// Learning C++
 // Challenge 01_03
-// Console Interaction, by Eduardo Corpeño 
+// Console Interaction, by Eduardo Corpeño
 
 #include <iostream>
 #include <string>
 
-int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
+int main() {
+  std::string name;
+  std::cout << "what is your name: ";
+  std::cin >> name;
+  std::cout << "Hello, " << name << std::endl;
 
-    std::cout << std::endl << std::endl;
-    return (0);
+  std::cout << std::endl << std::endl;
+  return (0);
 }
